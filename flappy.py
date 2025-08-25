c54017f9b71a409c9daf673fcac358e9c8f897d2
Sandeepa Oraon <sandeepa2004oraon@gmail.com>
2025-08-25 21:38:30 +0530

    aprites & audio

--- flappy.py
+++ flappy.py
@@ -1,2 +1,20 @@
 import pygame as pg
+from pygame.locals import *
 
+
+width= 600
+height= 600
+speed= 20
+run= True
+
+pg.init()
+pg.mixer.init()
+while run:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = False
+        if event.type == KEYDOWN:
+            if event.key == K_SPACE or event.key == K_UP:
+                bird.bump()
+                # pygame.mixer
+    keys= pg.key.get_pressed()
